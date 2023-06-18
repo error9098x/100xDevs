c9498e279cc99d0b119af77860d9b839481b1a6b
ProCodec <43810146+error9098x@users.noreply.github.com>
2023-06-18 19:44:07 +0530

    Update index.js

--- Week2/index.js
+++ Week2/index.js
@@ -1,26 +1,47 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+let noofreq = 0;
 
-function calsum(x){
-    sum = 0
-    for( i = 0 ; i<= x;i++){
-        sum+=i
+app.use(bodyParser.json())
+function middleware1(req, res, next){
+ 
+    noofreq+=1
+    console.log(noofreq)
+    next();
+}
+
+app.use(middleware1);
+
+function calculateSum(counter) {
+    var sum = 0;
+    for (var i =0 ; i<=counter; i++) {
+        sum = sum + i;
     }
-    return sum
+    return sum;
 }
 
-function handlerequest(req,res){
-    var calculatedsum = calsum(100);
-    console.log(calculatedsum)
-    var ans = "the sum is "+calculatedsum;
-    res.send(ans)
+function handleFirstRequest(req, res) {
 
+    var counter = req.body.counter;
+    console.log(req.body)
+    var calculatedSum = calculateSum(counter);
+
+    var answer = "the sum is " + calculatedSum;
+    res.set('X-My-Header', 'custom header value');
+    res.send(answer);
 }
-app.get('/', handlerequest)
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
+function createUser(req, res) {
+    res.send("hello world");
+}
 
-calculatedsum = calsum(10)
+app.post('/handleSum', handleFirstRequest);
+app.put('/createUser', createUser);
+
+function started() {
+    console.log(`Example app listening on port ${port}`)
+}
+
+app.listen(port, started);
